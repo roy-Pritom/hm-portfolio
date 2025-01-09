7e35b4d6b2fafe113a3bb7e7e72ec9c6e852ae83
roy-Pritom <pritombd.hitcse2020@gmail.com>
2025-01-09 15:06:07 +0600

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: "dream-school-landing-page",
+        name: "hm-portfolio",
         cwd: "/root/hm-portfolio",
         script: "node_modules/next/dist/bin/next",
         args: "start",
